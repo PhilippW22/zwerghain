9bb4aa60eaf04898c73e80e2c4e207e372dbc583
PhilippW22 <128072101+PhilippW22@users.noreply.github.com>
2026-03-18 10:23:23 +0100

    feat: update  ticker

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Hero from '@/components/sections/Hero'
 import Ticker from '@/components/ui/Ticker'
-import Features from '@/components/sections/Features'
 import EventHighlight from '@/components/sections/EventHighlight'
 import AboutUs from '@/components/sections/AboutUs'
 import FAQ from '@/components/sections/FAQ'
 import Tagline from '@/components/sections/Tagline'
 
 const tickerItems = [
-  'Öffnungszeiten: Montag Ruhetag | Dienstag bis Freitag 9 – 17:30 Uhr | Samstag, Sonntag & Feiertage 9 – 17 Uhr | Bitte beachtet: Am 22.03.2026 bleibt das Zwerghain am Nachmittag wegen einer Geburtstagsfeier geschlossen.',
+  'Öffnungszeiten: Montag Ruhetag | Dienstag bis Freitag 9 – 17:30 Uhr | Samstag, Sonntag & Feiertage 9 – 17 Uhr | Bitte beachtet: Am 21. & 22.03.2026 bleibt das Zwerghain am Nachmittag wegen einer Geburtstagsfeier geschlossen.',
 ]
 
 export default function HomePage() {
