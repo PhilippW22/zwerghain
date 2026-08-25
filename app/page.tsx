36f1e74eb3b5c16cd15b6a1ce3af24c224b5f5ae
PhilippW22 <128072101+PhilippW22@users.noreply.github.com>
2026-08-25 21:50:29 +0200

    update ticker

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import FAQ from '@/components/sections/FAQ'
 import Visit from '@/components/sections/Visit'
 
 const tickerItems = [
-  'Sommeröffnungszeiten: Di 14–18 Uhr · Mi–Fr 9–14 Uhr · Sa 9–18 Uhr · So 9-14 Uhr · Mo Ruhetag'
+  'Öffnungszeiten vom 24.08.–30.08.2026: Mo Ruhetag · Di 14–18 Uhr · Mi-Fr 9-14 Uhr · Sa & So wegen privaten Veranstaltung geschlossen'
   //  Sommeröffnungszeiten: Di 14–18 Uhr · Mi–Fr 9–14 Uhr · Sa 9–18 Uhr · So 9-14 Uhr · Mo Ruhetag · Hinweis: Am Donnerstag (06.08.) und Samstag (08.08.) bleibt das Zwerghain geschlossen.'
   //   Öffnungszeiten vom 09.–14.06.2026: Aufgrund privater Veranstaltungen schließen wir an einigen Tagen früher. Di 14–17:30 Uhr · Mi 9–17:30 Uhr · Do 9–14 Uhr · Fr 9–17:30 Uhr · Sa 9–13 Uhr · So 9–12:30 Uhr Sonntagsfrühstück nur mit Reservierung'
   //    Öffnungszeiten: Di 14–17:30 Uhr · Mi–Fr 9–17:30 Uhr · Sa & So 9–17 Uhr · Mo Ruhetag · Sonntagsfrühstück 9-13 Uhr nur mit Reservierung
