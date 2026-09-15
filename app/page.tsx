cc8fca3b79eb69d1327f7e35a1e7662c0e185e2c
PhilippW22 <128072101+PhilippW22@users.noreply.github.com>
2026-09-16 00:10:11 +0200

    trigger vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const tickerItems = [
   //   Öffnungszeiten vom 09.–14.06.2026: Aufgrund privater Veranstaltungen schließen wir an einigen Tagen früher. Di 14–17:30 Uhr · Mi 9–17:30 Uhr · Do 9–14 Uhr · Fr 9–17:30 Uhr · Sa 9–13 Uhr · So 9–12:30 Uhr Sonntagsfrühstück nur mit Reservierung'
   //    Öffnungszeiten: Di 14–17:30 Uhr · Mi–Fr 9–17:30 Uhr · Sa & So 9–17 Uhr · Mo Ruhetag · Sonntagsfrühstück 9-13 Uhr nur mit Reservierung
   //   · Hinweis: Am Samstag (dd.mm.) schließen wir ab 12:30 Uhr wegen einer geschlossenen Gesellschaft.
-  // Öffnungszeiten vom 31.08. – 06.09.2026: Mo Ruhetag · Di 14–18 Uhr · Mi-Fr 9-14 Uhr · Sa geschlossen · So 9-14 Uhr Sonntagsfrühstück bitte vorab reservieren
+  //  Öffnungszeiten vom 31.08. – 06.09.2026: Mo Ruhetag · Di 14–18 Uhr · Mi-Fr 9-14 Uhr · Sa geschlossen · So 9-14 Uhr Sonntagsfrühstück bitte vorab reservieren
   //  Öffnungszeiten vom 03.08.–09.08.2026: Mo Ruhetag · Di 14–18 Uhr · Mi 9-14 Uhr · Do geschlossen · Fr 9–14 Uhr · Sa geschlossen · So 9-14 Uhr
 ]
 
